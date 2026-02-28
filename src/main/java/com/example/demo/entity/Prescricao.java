4ac518e38eb3eb7b6cf06688e3886f1a1f0ff3e1
nitootin <phillip.profissional@gmail.com>
2026-02-28 12:02:37 -0300

    remoção de imports não utilizados

--- src/main/java/com/example/demo/entity/Prescricao.java
+++ src/main/java/com/example/demo/entity/Prescricao.java
@@ -2,8 +2,6 @@ package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
-import com.example.demo.enums.Perfil;
-
 import ch.qos.logback.core.status.Status;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
