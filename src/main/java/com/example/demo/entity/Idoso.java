73d790e622e305d8682be04b86c4624811cade77
FABIO ALEXANDRE FISCHER <fabio.fischer@alunos.sc.senac.br>
2026-03-18 20:27:14 -0300

    Criacao entidade vinculos criada

--- src/main/java/com/example/demo/entity/Idoso.java
+++ src/main/java/com/example/demo/entity/Idoso.java
@@ -1,11 +1,16 @@
 package com.example.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -19,7 +24,7 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
+@ToString(callSuper = true, exclude = { "instituicao", "contato", "vinculos" })
 @Table(name = "idoso")
 public class Idoso extends Usuario {
 
@@ -39,4 +44,7 @@ public class Idoso extends Usuario {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "contato_id", nullable = false, foreignKey = @ForeignKey(name = "fk_idoso_contato"))
     private Contato contato;
+
+    @OneToMany(mappedBy = "idoso", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Vinculo> vinculos = new ArrayList<>();
 }
