c2885f58c991b0e05532a1f4a7c3ef79ae8830d3
FABIO ALEXANDRE FISCHER <fabio.fischer@alunos.sc.senac.br>
2026-03-19 19:46:48 -0300

    DTO Instituicao criada

---
+++ src/main/java/com/example/demo/dtos/InstituicaoDTO.java
@@ -0,0 +1,25 @@
+package com.example.demo.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class InstituicaoDTO {
+    
+    private Long id;
+    private String nome;
+    private String cnpj;
+    private String bairro;
+    private String uf;
+    private Integer numero;
+    private String cep;
+
+
+}
