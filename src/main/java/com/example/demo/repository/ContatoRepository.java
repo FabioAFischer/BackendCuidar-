4991bf489b722c4a874b99e46815d9ff24dee464
nitootin <phillip.profissional@gmail.com>
2026-03-20 19:38:34 -0300

    Criado o repository de contato, com fundByTelefone

---
+++ src/main/java/com/example/demo/repository/ContatoRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Contato;
+
+@Repository
+public interface ContatoRepository extends JpaRepository<Contato, Integer>{
+
+      Optional<Contato> findByTelefone(String telefone);
+  
+} 
