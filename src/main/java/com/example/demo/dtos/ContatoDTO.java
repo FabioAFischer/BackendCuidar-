6c92ecfa7e8be4dd33e663b678765662d6da913c
nitootin <phillip.profissional@gmail.com>
2026-03-20 19:11:36 -0300

    Criação do DTO de contato

---
+++ src/main/java/com/example/demo/dtos/ContatoDTO.java
@@ -0,0 +1,21 @@
+package com.example.demo.dtos;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ContatoDTO {
+
+    private Integer id;
+    private String ddd;
+    private String telefone;
+    
+
+}
