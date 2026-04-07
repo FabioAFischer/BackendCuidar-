1804c02d9a281bbc3fe89ab16b3c98b6a337cd02
nitootin <phillip.profissional@gmail.com>
2026-04-07 20:13:04 -0300

    Criação da dto e inicialização do servide de remedio

---
+++ src/main/java/com/example/demo/services/RemedioService.java
@@ -0,0 +1,22 @@
+package com.example.demo.services;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dtos.InstituicaoDTO;
+import com.example.demo.entity.Instituicao;
+import com.example.demo.enums.Status;
+import com.example.demo.mappers.InstituicaoMapper;
+import com.example.demo.repository.InstituicaoRepository;
+
+@Service
+public class RemedioService {
+
+    private final RemedioRepository repository;
+
+
+    
+}
