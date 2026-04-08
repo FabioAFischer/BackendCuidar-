8b326f03de50f56c89ca0d20ebcc001a7a41dd0a
nitootin <phillip.profissional@gmail.com>
2026-04-08 20:18:15 -0300

    Criação do DTO de prescricao

---
+++ src/main/java/com/example/demo/dtos/PrescricaoDTO.java
@@ -0,0 +1,34 @@
+package com.example.demo.dtos;
+import java.time.LocalDateTime;
+
+import com.example.demo.entity.Idoso;
+import com.example.demo.entity.Remedio;
+import com.example.demo.enums.Status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PrescricaoDTO {
+
+    private int id;
+    private Remedio remedio;
+    private Idoso idoso;
+    private String medicamento;
+    private LocalDateTime data_criacao;
+    private Status status;
+    private LocalDateTime data_fim;
+    private Boolean necessario_jejum;
+    private String instrucao;
+    private Double intervalo;
+    private String dosagem;
+
+
+}
