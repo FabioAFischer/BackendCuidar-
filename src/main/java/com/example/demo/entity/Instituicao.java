3fe1fbeac1cd254882dcfba3e3093a3dc88fb374
FABIO ALEXANDRE FISCHER <fabio.fischer@alunos.sc.senac.br>
2026-04-24 20:59:30 -0300

    Revert "Implementação da dependencia spring-boot-starter-validation"
    This reverts commit 064840ab4e47bfec06e343a4cc19fc7138283224.

--- src/main/java/com/example/demo/entity/Instituicao.java
+++ src/main/java/com/example/demo/entity/Instituicao.java
@@ -3,8 +3,6 @@ package com.example.demo.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.validator.constraints.br.CNPJ;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,7 +10,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,7 +25,6 @@ import lombok.ToString;
 @Table(name = "instituicao")
 public class Instituicao extends Usuario {
 
-    @CNPJ
     @NotBlank(message = "CNPJ deve ser preenchido")
     @Column(length = 14, nullable = false, unique = true)
     private String cnpj;
@@ -40,7 +36,7 @@ public class Instituicao extends Usuario {
     private String uf;
 
     private int numero;
-    @Pattern(regexp = "\\d{8}", message = "CEP deve conter 8 dígitos")
+
     @Column(length = 8)
     private String cep;
 
