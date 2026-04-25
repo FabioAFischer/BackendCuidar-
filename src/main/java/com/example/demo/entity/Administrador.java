673f93a680305dd8a2ffaba01a99edeacd00067b
FabioAFischer <fabio.afischer@gmail.com>
2026-04-25 09:18:12 -0300

    Crud Admnistrador implementada

--- src/main/java/com/example/demo/entity/Administrador.java
+++ src/main/java/com/example/demo/entity/Administrador.java
@@ -1,19 +1,9 @@
 package com.example.demo.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,11 +15,12 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(callSuper = true)
 @Table(name = "administrador")
-public class Cuidador extends Usuario {
+public class Administrador extends Usuario {
 
     @NotBlank
-    @Column(length = 300, nullable = false)
+    @Column(length = 300, nullable = false, unique = true)
     private String email;
 
     @NotBlank
