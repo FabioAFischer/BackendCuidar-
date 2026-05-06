0c69f6d96de489a1c223b6eaafdee9b84b9fc32d
kevinamr <kevinamraulino@gmail.com>
2026-05-06 20:09:35 -0300

    criação crud vinculo

---
+++ src/main/java/com/example/demo/repository/VinculoRepository.java
@@ -0,0 +1,18 @@
+package com.example.demo.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Vinculo;
+
+@Repository
+public interface VinculoRepository extends JpaRepository<Vinculo, Integer> {
+
+    Page<Vinculo> findByIdosoId(Integer idosoId, Pageable pageable);
+
+    Page<Vinculo> findByCuidadorId(Integer cuidadorId, Pageable pageable);
+
+    boolean existsByIdosoIdAndCuidadorId(Integer idosoId, Integer cuidadorId);
+}
