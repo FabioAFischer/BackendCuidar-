7bfd79df1bba7fcda79d13d03ec8a7d87251fbee
Nitootin <phillip.profissional@gmail.com>
2026-05-12 14:12:47 -0300

    adicionar CRUD de prescrição no backend

---
+++ src/main/java/com/example/demo/repository/PrescricaoRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Prescricao;
+import com.example.demo.enums.Status;
+
+@Repository
+public interface PrescricaoRepository extends JpaRepository<Prescricao, Integer> {
+
+    Page<Prescricao> findByStatus(Status status, Pageable pageable);
+
+    Page<Prescricao> findByIdosoIdAndStatus(Integer idosoId, Status status, Pageable pageable);
+}
